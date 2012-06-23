18869b0643427ce8fcf5ede3ce30c44c46ee4aaf
hondacrx <hondacrx@gmail.com>
2012-06-23 15:29:29 -0400

    Forgot two values for delete.

--- sql/updates/23_06_2012_strawberry_opcodes.sql
+++ sql/updates/23_06_2012_strawberry_opcodes.sql
@@ -1,4 +1,4 @@
-DELETE FROM `opcodes` WHERE OpcodeValue IN (26887,);
+DELETE FROM `opcodes` WHERE OpcodeValue IN (26887,27669,3622);
 INSERT INTO `opcodes` (`OpcodeName`, `OpcodeValue`, `ClientBuild`, `StructureBuild`) VALUES 
 ('SMSG_WHO', 26887, 15595, 15595),
 ('CMSG_WHO', 27669, 15595, 15595),
