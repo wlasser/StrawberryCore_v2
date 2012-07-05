420164d3a9971b12721e3a5693b800b7162df26b
Netcho <kkotlarski@abv.bg>
2012-07-05 13:11:24 +0300

    Core/Battlefield: Add sql update

---
+++ sql/updates/2012_07_05_strawberry_opcodes.sql
@@ -0,0 +1,3 @@
+DELETE FROM `opcodes` WHERE `OpcodeValue` IN (1043,5542);
+INSERT INTO `opcodes` (`OpcodeName`, `OpcodeValue`, `ClientBuild`, `StructureBuild`) VALUES ('CMSG_BATTLEFIELD_MGR_QUEUE_INVITE_RESP', 1043, 15595, 15595);
+INSERT INTO `opcodes` (`OpcodeName`, `OpcodeValue`, `ClientBuild`, `StructureBuild`) VALUES ('SMSG_BATTLEFIELD_MGR_QUEUE_INVITE', 5542, 15595, 0);
