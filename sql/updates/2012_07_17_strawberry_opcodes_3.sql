373df0d6819e468c920740fc29bb87468a86820c
netcho <kkotlarski@abv.bg>
2012-07-17 23:39:43 +0300

    Add forgoten SQL update
    Signed-off-by: netcho <kkotlarski@abv.bg>

---
+++ sql/updates/2012_07_17_strawberry_opcodes_3.sql
@@ -0,0 +1,3 @@
+DELETE FROM `opcodes` WHERE `OpcodeValue` IN (31158,13491);
+INSERT INTO `opcodes` (`OpcodeName`, `OpcodeValue`, `ClientBuild`, `StructureBuild`) VALUES ('SMSG_BATTLEFIELD_MGR_QUEUE_REQ_RESP', 31158, 15595, 15595);
+INSERT INTO `opcodes` (`OpcodeName`, `OpcodeValue`, `ClientBuild`, `StructureBuild`) VALUES ('SMSG_BATTLEFIELD_MGR_STATE_CHANGED', 13491, 15595, 15595);
